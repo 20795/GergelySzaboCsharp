5ac6b33226a62b10dbac2a7fdb1b1c816c3df72e
20795 <20795g@gmail.com>
2018-03-22 10:32:53 +0100

    Godkänd nivå (hoppas jag)

--- GergelySzaboCsharp/Form1.Designer.cs
+++ GergelySzaboCsharp/Form1.Designer.cs
@@ -63,36 +63,43 @@ namespace GergelySzaboCsharp
         private void InitializeComponent()
         {
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
-
+            System.Windows.Forms.DataVisualization.Charting.Title title4 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title5 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title6 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-
+            this.chart5 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart6 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
-  
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart6)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
             chartArea1.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            //this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(12, 12);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
@@ -102,17 +109,16 @@ namespace GergelySzaboCsharp
             this.chart1.Size = new System.Drawing.Size(300, 300);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
-            this.chart1.Titles.Add ("Amsterdam - Private rooms");
-            //this.chart1.ChartareaschartArea1].AxisX.Title //sätter namnet på X-axeln
-            this.chart1.ChartAreas[0].AxisY.Title="Price per night";  //sätter namnet på Y-axeln
+            title1.Name = "Title1";
+            title1.Text = "Amsterdam - Private rooms";
+            this.chart1.Titles.Add(title1);
+            this.chart1.ChartAreas[0].AxisY.Title = "Price per night";  //sätter namnet på Y-axeln
             chartFilling(amsterdam, chart1);
             // 
             // chart2
             // 
             chartArea2.Name = "ChartArea1";
             this.chart2.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            //this.chart2.Legends.Add(legend2);
             this.chart2.Location = new System.Drawing.Point(318, 12);
             this.chart2.Name = "chart2";
             series2.ChartArea = "ChartArea1";
@@ -122,17 +128,16 @@ namespace GergelySzaboCsharp
             this.chart2.Size = new System.Drawing.Size(300, 300);
             this.chart2.TabIndex = 1;
             this.chart2.Text = "chart2";
-            this.chart2.Titles.Add("Barcelona - Private rooms");
-            //this.chart1.ChartareaschartArea1].AxisX.Title //sätter namnet på X-axeln
-            this.chart2.ChartAreas[0].AxisY.Title = "Price per night";  //sätter namnet på Y-axeln
+            title2.Name = "Title1";
+            title2.Text = "Barcelona - Private rooms";
+            this.chart2.Titles.Add(title2);
+            this.chart2.ChartAreas[0].AxisY.Title = "Price per night";
             chartFilling(barcelona, chart2);
             // 
             // chart3
             // 
             chartArea3.Name = "ChartArea1";
             this.chart3.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            //this.chart3.Legends.Add(legend3);
             this.chart3.Location = new System.Drawing.Point(625, 13);
             this.chart3.Name = "chart3";
             series3.ChartArea = "ChartArea1";
@@ -142,41 +147,90 @@ namespace GergelySzaboCsharp
             this.chart3.Size = new System.Drawing.Size(300, 300);
             this.chart3.TabIndex = 2;
             this.chart3.Text = "chart3";
-            this.chart3.Titles.Add("Boston - Private rooms");
-            //this.chart1.ChartareaschartArea1].AxisX.Title //sätter namnet på X-axeln
-            this.chart3.ChartAreas[0].AxisY.Title = "Price per night";  //sätter namnet på Y-axeln
+            title3.Name = "Title1";
+            title3.Text = "Boston - Private rooms";
+            this.chart3.Titles.Add(title3);
+            this.chart3.ChartAreas[0].AxisY.Title = "Price per night";
             chartFilling(boston, chart3);
             // 
             // chart4
             // 
             chartArea4.Name = "ChartArea1";
             this.chart4.ChartAreas.Add(chartArea4);
-            legend4.Name = "Legend1";
-            //this.chart4.Legends.Add(legend4);
             this.chart4.Location = new System.Drawing.Point(12, 329);
             this.chart4.Name = "chart4";
             series4.ChartArea = "ChartArea1";
             series4.Legend = "Legend1";
             series4.Name = "Series1";
+            series4.ChartType = SeriesChartType.Point;
             this.chart4.Series.Add(series4);
-            this.chart4.Series["Series1"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
-
-            this.chart4.Size = new System.Drawing.Size(912, 300);
+            this.chart4.Size = new System.Drawing.Size(300, 300);
             this.chart4.TabIndex = 3;
             this.chart4.Text = "chart4";
+
+            title4.Name = "Title1";
+            title4.Text = "Overall Satisfaction < 4.5 / Price (Amsterdam)";
+            this.chart4.Titles.Add(title4);
+
+            this.chart4.ChartAreas[0].AxisY.Title = "Overall Satisfaction";
+            this.chart4.ChartAreas[0].AxisX.Title = "Price per night";
             scatterplotFilling(amsterdam, chart4);
-            scatterplotFilling(barcelona, chart4);
-            scatterplotFilling(boston, chart4);
-            this.chart4.Titles.Add("Overall Satisfaction < 4.5 / Price");
-            this.chart4.ChartAreas[0].AxisX.Title = "Price per night";//sätter namnet på X-axeln
-            this.chart4.ChartAreas[0].AxisY.Title = "Overall Satisfaction";  //sätter namnet på Y-axeln
+            // 
+            // chart5
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart5.ChartAreas.Add(chartArea5);
+            legend1.Name = "Legend1";
+            //this.chart5.Legends.Add(legend1);
+            this.chart5.Location = new System.Drawing.Point(319, 329);
+            this.chart5.Name = "chart5";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            series5.ChartType = SeriesChartType.Point;
+            this.chart5.Series.Add(series5);
+            this.chart5.Size = new System.Drawing.Size(300, 300);
+            this.chart5.TabIndex = 4;
+            this.chart5.Text = "chart5";
+
+            title5.Name = "Title1";
+            title5.Text = "Overall Satisfaction < 4.5 / Price (Barcelona)";
+            this.chart5.Titles.Add(title5);
+
+            this.chart5.ChartAreas[0].AxisY.Title = "Overall Satisfaction";
+            this.chart5.ChartAreas[0].AxisX.Title = "Price per night";
+            scatterplotFilling(barcelona, chart5);
+            // 
+            // chart6
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart6.ChartAreas.Add(chartArea6);
+            legend2.Name = "Legend1";
+            //this.chart6.Legends.Add(legend2);
+            this.chart6.Location = new System.Drawing.Point(625, 329);
+            this.chart6.Name = "chart6";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            series6.ChartType = SeriesChartType.Point;
+            this.chart6.Series.Add(series6);
+            this.chart6.Size = new System.Drawing.Size(300, 300);
+            this.chart6.TabIndex = 5;
+            this.chart6.Text = "chart6";
+            title6.Name = "Title1";
+            title6.Text = "Overall Satisfaction < 4.5 / Price (Boston)";
+            this.chart6.Titles.Add(title6);
+            this.chart6.ChartAreas[0].AxisY.Title = "Overall Satisfaction";
+            this.chart6.ChartAreas[0].AxisX.Title = "Price per night";
+            scatterplotFilling(boston, chart6);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(940, 640);
-            
+            this.Controls.Add(this.chart6);
+            this.Controls.Add(this.chart5);
             this.Controls.Add(this.chart4);
             this.Controls.Add(this.chart3);
             this.Controls.Add(this.chart2);
@@ -187,7 +241,8 @@ namespace GergelySzaboCsharp
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
-            
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart6)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -198,7 +253,8 @@ namespace GergelySzaboCsharp
         private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
-
+        private Chart chart5;
+        private Chart chart6;
     }
 }
 
